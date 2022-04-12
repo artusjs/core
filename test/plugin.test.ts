785dc9ffdb81a738a7952dc388d25cc64d07a1ef
hyj1991 <yeekwanvong@gmail.com>
2022-04-12 18:07:16 +0800

    fix: :tada: shouldn't throw if optional dependecies missing (#24)
    * fix: shouldn't throw if optional dependecies missing
    
    * add test

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -57,5 +57,63 @@ describe('test/app.test.ts', () => {
         await PluginFactory.createFromConfig(mockPluginConfig)
       }).rejects.toThrowError(new Error(`There is a cycle in the dependencies, wrong plugin is plugin-wrong-a,plugin-wrong-b.`));
     });
+
+    it('should throw if dependence missing', async () => {
+      const mockPluginConfig = {
+        'plugin-a': {
+          enable: true,
+          path: path.resolve(__dirname, `${pluginPrefix}/plugin-a`),
+        }
+      }
+      expect(async () => {
+        await PluginFactory.createFromConfig(mockPluginConfig)
+      }).rejects.toThrowError(new Error(`Plugin plugin-a need have dependence: plugin-b.`));
+    });
+
+    it('should throw if dependence disabled', async () => {
+      const mockPluginConfig = {
+        'plugin-a': {
+          enable: true,
+          path: path.resolve(__dirname, `${pluginPrefix}/plugin-a`),
+        },
+        'plugin-b': {
+          enable: false,
+          path: path.resolve(__dirname, `${pluginPrefix}/plugin-b`),
+        },
+        'plugin-c': {
+          enable: true,
+          path: path.resolve(__dirname, `${pluginPrefix}/plugin-c`),
+        }
+      }
+      expect(async () => {
+        await PluginFactory.createFromConfig(mockPluginConfig)
+      }).rejects.toThrowError(new Error(`Plugin plugin-a need have dependence: plugin-b.`));
+    });
+
+    it('should not throw if optional dependence missing', async () => {
+      const mockPluginConfig = {
+        'plugin-d': {
+          enable: true,
+          path: path.resolve(__dirname, `${pluginPrefix}/plugin-d`),
+        }
+      }
+      let warnMessage = '';
+
+      // mock warn
+      const originWarn = console.warn;
+      console.warn = function (msg) {
+        warnMessage = msg;
+      }
+      const pluginList = await PluginFactory.createFromConfig(mockPluginConfig);
+      expect(pluginList.length).toEqual(1);
+      pluginList.forEach(plugin => {
+        expect(plugin).toBeInstanceOf(ArtusPlugin);
+        expect(plugin.enable).toBeTruthy();
+      });
+      expect(warnMessage === 'Plugin plugin-d need have optional dependence: plugin-e.').toBeTruthy();
+
+      // restore warn
+      console.warn = originWarn;
+    });
   });
 });
