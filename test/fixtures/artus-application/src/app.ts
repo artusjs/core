d323ec837917ee3d4e05c96149b001dc78b472ad
hyj1991 <yeekwanvong@gmail.com>
2022-04-20 21:49:50 +0800

    start app with framework

--- test/fixtures/artus-application/src/app.ts
+++ test/fixtures/artus-application/src/app.ts
@@ -1,27 +1,10 @@
-import { FrameworkBar } from '../../frameworks/bar/src';
-import { readdir } from 'fs/promises';
-import path from 'path';
+import { ArtusApplication, Manifest } from "../../../../src";
 
-class MyArtusApplication extends FrameworkBar {
-  async scanDir(dir) {
-    const files = await readdir(dir);
-    for (const file of files) {
-      super.loadFile(path.join(dir, file));
-    }
+export class AppBoot {
+  static async main(manifest: Manifest) {
+    const app = new ArtusApplication();
+    await app.load(manifest);
+    await app.run();
+    return app;
   }
-
-  async loadFile() {
-    const controller = path.join(__dirname, 'controller');
-    await this.scanDir(controller);
-
-    const config = path.join(__dirname, 'config');
-    await this.scanDir(config);
-  }
-}
-
-export async function main() {
-  const app = new MyArtusApplication();
-  await app.loadFile();
-  await app.run();
-  return app;
 }
--- test/framework.test.ts
+++ test/framework.test.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import { Scanner } from '../src/scanner';
 import path from 'path';
+import axios from 'axios';
+import assert from 'assert';
 
 describe('test/framework.test.ts', () => {
   it('should run succeed', async () => {
@@ -8,17 +10,15 @@ describe('test/framework.test.ts', () => {
       const scanner = new Scanner({ needWriteFile: false, extensions: ['.ts', '.js', '.json'] });
       const manifest = await scanner.scan(path.resolve(__dirname, './fixtures/artus-application'));
       console.log('manifest', manifest);
-      // const {
-      //   main,
-      // } = await import('./fixtures/artus-application/src/app');
-      // const app = await main();
+      const { AppBoot } = await import('./fixtures/artus-application/src/app');
+      const app = await AppBoot.main(manifest);
       // assert(app.isListening());
-      // const port = app.config?.port;
+      const port = app.config?.port;
       // assert(port === 3003);
-      // const testResponse = await axios.get(`http://127.0.0.1:${port}/home`);
-      // assert(testResponse.status === 200);
-      // assert(testResponse.data.title === 'Hello Artus from application');
-      // await app.close();
+      const testResponse = await axios.get(`http://127.0.0.1:${port}/home`);
+      assert(testResponse.status === 200);
+      assert(testResponse.data.title === 'Hello Artus from application');
+      await app.close();
       // assert(!app.isListening());
     } catch (err) {
       console.log(err);
