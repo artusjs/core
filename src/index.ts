a8eb3c037df81730ce0cf56211d21f0aae74c6a0
hyj1991 <yeekwanvong@gmail.com>
2022-04-28 20:26:41 +0800

    fix: export missing files

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,9 @@ export * from './exception';
 export * from './plugin';
 export * from './application';
 export * from './scanner';
+export * from './decorator';
+export * from './types';
+export * from './constraints';
 
 import Trigger from './trigger';
 export { Trigger };
