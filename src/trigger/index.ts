3adda4984f1bab3cc5c2ae2b34263c9c5e53dc17
JerrysShan <1670303003@qq.com>
2022-06-01 18:20:05 +0800

    chore: open trigger extend

--- src/trigger/index.ts
+++ src/trigger/index.ts
@@ -1,11 +1,11 @@
 import { ExecutionContainer, Inject } from '@artus/injection';
 import { Input, Context, MiddlewareInput, Pipeline, Output } from '@artus/pipeline';
 import { ArtusInjectEnum } from '../constant';
-import { Application } from '../types';
+import { Application, TriggerType } from '../types';
 import { DefineTrigger } from './decorator';
 
 @DefineTrigger()
-export default class Trigger {
+export default class Trigger implements TriggerType {
   private pipeline: Pipeline;
 
   @Inject(ArtusInjectEnum.Application)
@@ -21,14 +21,14 @@ export default class Trigger {
     this.pipeline.use(middleware);
   }
 
-  async initContext(input: Input): Promise<Context> {
-    const ctx = new Context(input, new Output());
-    ctx.container = new ExecutionContainer(ctx, this.app.getContainer())
+  async initContext(input: Input, output: Output): Promise<Context> {
+    const ctx = new Context(input, output);
+    ctx.container = new ExecutionContainer(ctx, this.app.getContainer());
     return ctx;
   }
 
-  async startPipeline(input: Input = new Input()): Promise<Context> {
-    const ctx = await this.initContext(input);
+  async startPipeline(input: Input = new Input(), output = new Output()): Promise<Context> {
+    const ctx = await this.initContext(input, output);
     await this.pipeline.run(ctx);
     return ctx;
   }
