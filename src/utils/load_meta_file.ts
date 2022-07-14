dd3c5126c3e6f64892fc850ac56876afa8154ab9
Ziheng Gao <noahgaocn@outlook.com>
2022-07-14 17:39:12 +0800

    refactor(utils): loadMetaFile check condition

--- src/utils/load_meta_file.ts
+++ src/utils/load_meta_file.ts
@@ -2,11 +2,8 @@ import { ManifestItem } from '../loader/types';
 import compatibleRequire from './compatible_require';
 
 export const loadMetaFile = async <T = Record<string, any>>(item: ManifestItem): Promise<T> => {
-  if (item.extname !== '.json') {
-    throw new Error(`[loadMetaFile] ${item.path} is not a json file.`);
-  }
   const metaObject = await compatibleRequire(item.path);
-  if (!metaObject) {
+  if (!metaObject || typeof metaObject !== 'object') {
     throw new Error(`[loadMetaFile] ${item.path} is not a valid json file.`);
   }
   return metaObject;
