34633d7213bdf5eb2487a91357f1f030c6a749f6
Ziheng Gao <noahgaocn@outlook.com>
2022-10-19 18:19:51 +0800

    feat(exception): exception filterr default duplicated check

--- src/loader/impl/exception_filter.ts
+++ src/loader/impl/exception_filter.ts
@@ -1,15 +1,15 @@
 import { DefineLoader } from '../decorator';
 import { ManifestItem } from '../types';
 import ModuleLoader from './module';
-import { ArtusStdError, EXCEPTION_FILTER_MAP_INJECT_ID, EXCEPTION_FILTER_METADATA_KEY } from '../../exception';
+import { ArtusStdError, EXCEPTION_FILTER_DEFAULT_SYMBOL, EXCEPTION_FILTER_MAP_INJECT_ID, EXCEPTION_FILTER_METADATA_KEY } from '../../exception';
 import { Constructable } from '@artus/injection';
-import { ExceptionFilterType } from '../../exception/types';
+import { ExceptionFilterMapType, ExceptionFilterType, ExceptionIdentifier } from '../../exception/types';
 
 @DefineLoader('exception_filter')
 class ExceptionFilterLoader extends ModuleLoader {
   async load(item: ManifestItem) {
     // Get or Init Exception Filter Map
-    let filterMap: Map<string|Constructable<Error>, Constructable<ExceptionFilterType>> = this.container.get(EXCEPTION_FILTER_MAP_INJECT_ID, {
+    let filterMap: ExceptionFilterMapType = this.container.get(EXCEPTION_FILTER_MAP_INJECT_ID, {
       noThrow: true,
     });
     if (!filterMap) {
@@ -24,7 +24,7 @@ class ExceptionFilterLoader extends ModuleLoader {
     for (let i = 0; i < clazzList.length; i++) {
       const filterClazz = clazzList[i];
       const filterMeta: {
-        targetErr: string|Constructable<Error>
+        targetErr: ExceptionIdentifier
       } = Reflect.getOwnMetadata(EXCEPTION_FILTER_METADATA_KEY, filterClazz);
 
       if (!filterMeta) {
@@ -34,12 +34,18 @@ class ExceptionFilterLoader extends ModuleLoader {
       let { targetErr } = filterMeta;
       if (filterMap.has(targetErr)) {
         // check duplicated
-        const targetErrName = typeof targetErr === 'string' ? targetErr : (targetErr.name || targetErr);
-        throw new Error(`the ExceptionFilter for ${targetErrName} is duplicated`);
+        if (targetErr === EXCEPTION_FILTER_DEFAULT_SYMBOL) {
+          throw new Error('the Default ExceptionFilter is duplicated');
+        }
+        let targetErrName = targetErr;
+        if (typeof targetErr !== 'string' && typeof targetErr !== 'symbol') {
+          targetErrName = targetErr.name || targetErr;
+        }
+        throw new Error(`the ExceptionFilter for ${String(targetErrName)} is duplicated`);
       }
 
       if (
-        typeof targetErr !== 'string' && // Decorate with a error class
+        typeof targetErr !== 'string' && typeof targetErr !== 'symbol' && // Decorate with a error class
         Object.prototype.isPrototypeOf.call(ArtusStdError.prototype, targetErr.prototype) && // the class extends ArtusStdError
         typeof targetErr['code'] === 'string' // Have static property `code` defined by string
       ) {
