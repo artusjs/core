efac7084c4f56b723fcfac71e3a996c59eb7b638
Noah Gao <noahgaocn@outlook.com>
2023-04-24 19:14:02 +0800

    feat(scanner): refactor scanner workflow by task-queue (#242)

--- src/loader/types.ts
+++ src/loader/types.ts
@@ -1,14 +1,33 @@
 import { Container } from '@artus/injection';
 import { ScanPolicy } from '../constant';
-import { PluginConfigItem } from '../plugin/types';
+import { PluginConfigItem, PluginDependencyItem } from '../plugin/types';
 
-interface Manifest {
+export interface Manifest {
   items: ManifestItem[];
   pluginConfig?: Record<string, PluginConfigItem>;
   relative?: boolean;
 }
+export type ManifestEnvMap = Record<string, Manifest>;
 
-interface ManifestItem<LoaderState = unknown> extends Record<string, any> {
+export interface ManifestV2PluginConfigItem {
+  enable: boolean;
+  refName: string;
+}
+export type ManifestV2PluginConfig = Record<string, ManifestV2PluginConfigItem>;
+export interface ManifestV2RefMapItem {
+  packageVersion?: string;
+  dependencies?: PluginDependencyItem[];
+  items: ManifestItem[];
+}
+
+export interface ManifestV2 {
+  version: '2';
+  pluginConfig: Record<string, Record<string, ManifestV2PluginConfigItem>>;
+  refMap: Record<string, ManifestV2RefMapItem>;
+  relative: boolean;
+}
+
+export interface ManifestItem<LoaderState = unknown> extends Record<string, any> {
   path: string;
   extname: string;
   filename: string;
@@ -18,7 +37,7 @@ interface ManifestItem<LoaderState = unknown> extends Record<string, any> {
   loaderState?: LoaderState;
 }
 
-interface LoaderFindOptions {
+export interface LoaderFindOptions {
   filename: string;
   root: string;
   baseDir: string;
@@ -26,32 +45,23 @@ interface LoaderFindOptions {
   policy?: ScanPolicy;
 }
 
-interface LoaderFindResult {
+export interface LoaderFindResult {
   loaderName: string;
   loaderState?: unknown;
 }
 
-interface LoaderHookUnit {
+export interface LoaderHookUnit {
   before?: Function;
   after?: Function;
 }
 
-interface LoaderConstructor {
-  new (container: Container): Loader;
+export interface LoaderConstructor {
+  new(container: Container): Loader;
   is?(opts: LoaderFindOptions): Promise<boolean>;
   onFind?(opts: LoaderFindOptions): Promise<any>;
 }
-interface Loader {
+export interface Loader {
   state?: any;
   load(item: ManifestItem): Promise<any>;
 }
 
-export {
-  Manifest,
-  ManifestItem,
-  LoaderHookUnit,
-  LoaderConstructor,
-  Loader,
-  LoaderFindOptions,
-  LoaderFindResult,
-};
