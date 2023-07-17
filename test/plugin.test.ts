708889bd8b1bfbab40df3e73d87b650ffc87fdbe
Noah Gao <noahgaocn@outlook.com>
2023-07-17 11:54:00 +0800

    fix(plugin): optional plugin also need to calc order (#256)
    * fix(plugin): optional plugin also need to calc order
    
    * test: update snapshot

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -201,10 +201,91 @@ describe('test/app.test.ts', () => {
         expect(plugin).toBeInstanceOf(Plugin);
         expect(plugin.enable).toBeTruthy();
       });
-      expect(mockWarnFn).toBeCalledWith(`Plugin plugin-d need have optional dependency: plugin-e.`);
+      expect(mockWarnFn).toBeCalledWith(`Plugin plugin-d need have optional dependency: plugin-c.`);
 
       // restore warn
       console.warn = originWarn;
+    });
+
+    it('should not throw if optional dependence disabled', async () => {
+      const mockPluginConfig = {
+        'plugin-c': {
+          enable: false,
+          path: path.resolve(__dirname, `${pluginPrefix}/plugin_c`),
+          manifest: {
+            pluginMeta: {
+              path: path.resolve(__dirname, `${pluginPrefix}/plugin_c/meta.js`),
+              extname: '.js',
+              filename: 'meta.js',
+            },
+          },
+        },
+        'plugin-d': {
+          enable: true,
+          path: path.resolve(__dirname, `${pluginPrefix}/plugin_d`),
+          manifest: {
+            pluginMeta: {
+              path: path.resolve(__dirname, `${pluginPrefix}/plugin_d/meta.js`),
+              extname: '.js',
+              filename: 'meta.js',
+            },
+          },
+        },
+      };
+
+      // mock warn
+      const originWarn = console.warn;
+      const mockWarnFn = jest.fn();
+      console.warn = mockWarnFn;
+      const pluginList = await PluginFactory.createFromConfig(mockPluginConfig, {
+        logger: new Logger(),
+      });
+      expect(pluginList.length).toEqual(1);
+      pluginList.forEach(plugin => {
+        expect(plugin).toBeInstanceOf(Plugin);
+        expect(plugin.enable).toBeTruthy();
+      });
+      expect(mockWarnFn).toBeCalledWith(`Plugin plugin-d need have optional dependency: plugin-c.`);
+
+      // restore warn
+      console.warn = originWarn;
+    });
+
+    it('should calc order if optional dependence enabled', async () => {
+      const mockPluginConfig = {
+        'plugin-d': {
+          enable: true,
+          path: path.resolve(__dirname, `${pluginPrefix}/plugin_d`),
+          manifest: {
+            pluginMeta: {
+              path: path.resolve(__dirname, `${pluginPrefix}/plugin_d/meta.js`),
+              extname: '.js',
+              filename: 'meta.js',
+            },
+          },
+        },
+        'plugin-c': {
+          enable: true,
+          path: path.resolve(__dirname, `${pluginPrefix}/plugin_c`),
+          manifest: {
+            pluginMeta: {
+              path: path.resolve(__dirname, `${pluginPrefix}/plugin_c/meta.js`),
+              extname: '.js',
+              filename: 'meta.js',
+            },
+          },
+        },
+      };
+
+      const pluginList = await PluginFactory.createFromConfig(mockPluginConfig, {
+        logger: new Logger(),
+      });
+      expect(pluginList.length).toEqual(2);
+      pluginList.forEach(plugin => {
+        expect(plugin).toBeInstanceOf(Plugin);
+        expect(plugin.enable).toBeTruthy();
+      });
+      expect(pluginList.map(plugin => plugin.name)).toStrictEqual(['plugin-c', 'plugin-d']);
     });
   });
 });
